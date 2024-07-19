cf1677c17c1bcea64dc12c41b2eeef8a73e5ba2a
rhnnss <muhrayhanns04@gmail.com>
2024-07-19 16:41:20 +0700

    refactor: Refactor getTimeRemaining function for improved readability and performance

--- index.js
+++ index.js
@@ -40,24 +40,22 @@ export function formatNumericValue(value, options = {}) {
 
 export const getTimeRemaining = (endtime) => {
 	const total = Date.parse(endtime) - Date.parse(new Date());
-	const seconds = Math.max(Math.floor((total / 1000) % 60), 0)
-		.toString()
-		.padStart(2, "0");
-	const minutes = Math.max(Math.floor((total / 1000 / 60) % 60), 0)
-		.toString()
-		.padStart(2, "0");
-	const hours = Math.max(Math.floor((total / (1000 * 60 * 60)) % 24), 0)
-		.toString()
-		.padStart(2, "0");
-	const days = Math.max(Math.floor(total / (1000 * 60 * 60 * 24)), 0)
-		.toString()
-		.padStart(2, "0");
+	const timeCalc = (unit) =>
+		Math.max(
+			Math.floor(
+				(total / unit) %
+					(unit === 1000 ? 60 : unit === 1000 * 60 ? 60 : 24)
+			),
+			0
+		)
+			.toString()
+			.padStart(2, "0");
 
 	return {
 		total,
-		days,
-		hours,
-		minutes,
-		seconds,
+		days: timeCalc(1000 * 60 * 60 * 24),
+		hours: timeCalc(1000 * 60 * 60),
+		minutes: timeCalc(1000 * 60),
+		seconds: timeCalc(1000),
 	};
 };
