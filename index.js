f2a80fcae83050dfefce0e1c03bb1f944b03e0b9
rhnnss <muhrayhanns04@gmail.com>
2024-07-19 16:24:56 +0700

    feat: Add getTimeRemaining function for calculating time difference

--- index.js
+++ index.js
@@ -37,3 +37,27 @@ export function formatNumericValue(value, options = {}) {
 
 	return formattedResult;
 }
+
+export const getTimeRemaining = (endtime) => {
+	const total = Date.parse(endtime) - Date.parse(new Date());
+	const seconds = Math.max(Math.floor((total / 1000) % 60), 0)
+		.toString()
+		.padStart(2, "0");
+	const minutes = Math.max(Math.floor((total / 1000 / 60) % 60), 0)
+		.toString()
+		.padStart(2, "0");
+	const hours = Math.max(Math.floor((total / (1000 * 60 * 60)) % 24), 0)
+		.toString()
+		.padStart(2, "0");
+	const days = Math.max(Math.floor(total / (1000 * 60 * 60 * 24)), 0)
+		.toString()
+		.padStart(2, "0");
+
+	return {
+		total,
+		days,
+		hours,
+		minutes,
+		seconds,
+	};
+};
